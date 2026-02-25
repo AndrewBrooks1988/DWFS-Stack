b8be89c2fd782140726beaf03347fa142d500d94
AndrewBrooks1988 <andrewjbrooks1988@gmail.com>
2026-02-25 15:31:34 +1100

    Add WPF project
    Added WPF User Interface project and set up MVVM

---
+++ DWFSWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using DWFSWPFUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DWFSWPFUserInterface
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
