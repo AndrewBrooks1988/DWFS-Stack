1e208dd7233192fb70d9147a6a350d7689f1b1ac
AndrewBrooks1988 <andrewjbrooks1988@gmail.com>
2026-02-27 00:24:23 +1100

    Register DB Tables
    The tables for the basic register system

---
+++ DWFSDatabase/dbo/Tables/Sale.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CashierId] NVARCHAR(128) NOT NULL, 
+    [SaleDate] DATETIME2 NOT NULL, 
+    [SubTotal] MONEY NOT NULL,
+    [Shipping] MONEY NULL,
+    [Tax] MONEY NOT NULL, 
+    [Total] MONEY NOT NULL
+)
