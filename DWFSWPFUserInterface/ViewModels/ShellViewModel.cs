bb7f31f245c99647a9d961fefdfec8bd4ff2addd
AndrewBrooks1988 <andrewjbrooks1988@gmail.com>
2026-03-05 00:23:05 +1100

    Replace Container Reference in ShellVM
    Replace Container reference with the IoC reference instead

--- DWFSWPFUserInterface/ViewModels/ShellViewModel.cs
+++ DWFSWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -13,30 +13,18 @@ namespace DWFSWPFUserInterface.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
-            SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.SubscribeOnPublishedThread(this);
 
-            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            //ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
         }
 
-        //public task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
-        //{
-        //    ActivateItemAsync(_salesVM, cancellationToken);
-        //}
-
-
-        //public void Handle(LogOnEvent message)
-        //{
-        //    ActivateItemAsync(_salesVM);
-        //}
         Task IHandle<LogOnEvent>.HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
         {
             ActivateItemAsync(_salesVM);
